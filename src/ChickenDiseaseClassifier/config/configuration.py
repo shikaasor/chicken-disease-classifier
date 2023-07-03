5991616adf6e4069c62f5caa8c0374f9bfeabc5c
shikaasor <asor.tar23@gmail.com>
2023-07-03 12:47:41 +0100

    dvc added

--- src/ChickenDiseaseClassifier/config/configuration.py
+++ src/ChickenDiseaseClassifier/config/configuration.py
@@ -1,10 +1,12 @@
 from ChickenDiseaseClassifier.constants import *
 import os
+from pathlib import Path
 from ChickenDiseaseClassifier.utils.common import read_yaml, create_directories
 from ChickenDiseaseClassifier.entity.config_entity import (DataIngestionConfig,
                                                            PrepareBaseModelConfig,
                                                            PrepareCallbacksConfig,
-                                                           TrainingConfig)
+                                                           TrainingConfig,
+                                                           EvaluationConfig)
 
 
 
@@ -98,3 +100,12 @@ class ConfigurationManager:
         )
 
         return training_config
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model=Path("artifacts/training/model.h5"),
+            training_data=Path("artifacts/data_ingestion/Chicken-fecal-images"),
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE
+        )
+        return eval_config
